e3a594c032b5e9e3ccc4e613b8242878cf7cdbca
ogbk <ogbu.k.olu@gmail.com>
2020-07-21 23:50:08 +0100

    functions working in products

--- client/app/util/functions.js
+++ client/app/util/functions.js
@@ -1,5 +1,35 @@
+// @flow
+
+import type { DiscountType, ItemType, ProductsType, StoreType } from './datatypes';
+
 const getOnlyNumber = (value) => (
   Number(value.replace(/[^0-9+]/g, ''))
 );
 
-export { getOnlyNumber };
+const findProductByName = ({ products }: StoreType, productName: string) => (
+  products.find(({ name }) => (name === productName))
+);
+
+const getProductIndex = ({ products }: StoreType, productName: string) => (
+  products.findIndex(({ name }) => (name === productName))
+);
+
+const productExists = ({ products }: StoreType, productName: string) => (
+  products.some(({ name }) => (name === productName))
+);
+
+const updateProductByName = (store: StoreType, newProduct: ItemType) => {
+  const oldIndex = getProductIndex(store, newProduct.name);
+  const newStore = { ...store };
+
+  newStore.splice(oldIndex, 1, newProduct);
+  return newStore;
+};
+
+export {
+  getOnlyNumber,
+  findProductByName,
+  getProductIndex,
+  productExists,
+  updateProductByName,
+};

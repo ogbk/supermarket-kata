53a909990a744755c898e97f46ed5757b07aaf73
ogbk <ogbu.k.olu@gmail.com>
2020-07-22 20:34:29 +0100

    products appear in <Cart/> in creation order

--- jest_testing/__tests__/Cart-Config.js
+++ jest_testing/__tests__/Cart-Config.js
@@ -11,7 +11,7 @@ let app;
 
 describe('Create new product', () => {
 
-  beforeAll(() => {
+  beforeEach(() => {
     app = mount(<App />);
   });
 
@@ -42,6 +42,33 @@ describe('Create new product', () => {
     app.find('.config-abort').simulate('click');
     expect(app.find(Config).exists()).toBe(false);
     expect(app.find(Cart).exists()).toBe(true);
+  });
+
+  test('create 2 products', () => {
+    const PRODUCT_SUFFIX = ': 0 item'; // exactly what follows a product name when listed in cart
+
+    const select_option = app.find('.app-config > select');
+
+    expect(app.find('.cart-item').children()).toBeUndefined;
+
+    select_option.simulate('change', { target: { value:'' } });
+    app.find('button.execute-action').simulate('click');
+    app.find('input[name="product-name"]').simulate('change', {target: {value: 'dummy1'}});
+    app.find('.config-save').simulate('click');
+
+    select_option.simulate('change', { target: { value:'' } });
+    app.find('button.execute-action').simulate('click');
+    app.find('input[name="product-name"]').simulate('change', {target: {value: 'dummy2'}});
+    app.find('.config-save').simulate('click');
+
+    expect(app.find(Config).exists()).toBe(false);
+    expect(app.find(Cart).exists()).toBe(true);
+
+    const cartItems = app.find('.cart .content .cart-item');
+    expect(cartItems).toHaveLength(2);
+    expect(cartItems.at(0).find('span').text()).toBe('dummy1'+PRODUCT_SUFFIX);
+    expect(cartItems.at(1).find('span').text()).toBe('dummy2'+PRODUCT_SUFFIX);
+
   });
 
 

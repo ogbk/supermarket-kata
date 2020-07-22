2ebc60451d83b6f6874820d4a83e0098a1a49a9f
ogbk <ogbu.k.olu@gmail.com>
2020-07-22 11:56:51 +0100

    add <Receipt/>

--- client/app/components/Receipt.js
+++ client/app/components/Receipt.js
@@ -5,17 +5,40 @@ import React, { useEffect, useState } from 'react';
 // import type { CartType } from '../util/datatypes';
 
 type Props = {
+  store: StoreType,
 }
 
-const Receipt = ({ }: Props) => {
+const Receipt = ({ store }: Props) => {
+  const { products } = store;
+  let subTotal = 0;
+  let totalSavings = 0;
+  let totalToPay = 0;
+
+  products.forEach((thisProduct) => {
+    const { fullPrice, savings } = thisProduct;
+    subTotal += fullPrice;
+    if (savings > 0) {
+      totalSavings += savings;
+    }
+  });
+  totalToPay = subTotal - totalSavings;
+
+  const value = `
+  Subtotal -------- ${subTotal}
+
+  Total savings --- ${totalSavings}
+
+  Total to pay ---- ${totalToPay}
+  `;
 
   return (
     <div className="receipt">
       <span className="title">RECEIPT</span>
       <div className="content">
-        <textarea readOnly>
-          RECEIPT - CONTENT
-        </textarea>
+        <textarea
+          readOnly
+          value={value}
+        />
       </div>
     </div>
   );
